124fc9f70ba3f14220869b5f9c03ef974273189a
Martin Wirall <martin@wirallinteractive.com.ar>
2014-06-16 15:19:07 -0300

    main

--- app/protected/config/main.php
+++ app/protected/config/main.php
@@ -101,7 +101,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=fabogesic',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => 'root',
+			'password' => 'sqlw1r4ll',
 			'charset' => 'utf8',
 		),
 		'errorHandler'=>array(
