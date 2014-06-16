c7f11630baa0114da5cb90ee5fe30f6fd70d1795
Tomoy <moyanotomasi@gmail.com>
2014-06-16 15:23:09 -0300

    main Correct Username

--- app/protected/config/main.php
+++ app/protected/config/main.php
@@ -100,7 +100,7 @@ return array(
 		'db'=>array(
 			'connectionString' => 'mysql:host=localhost;dbname=fabogesic',
 			'emulatePrepare' => true,
-			'username' => 'root3',
+			'username' => 'root',
 			'password' => 'root',
 			'charset' => 'utf8',
 		),
